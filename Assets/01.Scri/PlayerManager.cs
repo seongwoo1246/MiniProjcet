c2439d1c66907b20aeec95bea282c31f566e9f8a
seongwoo1246 <seongau717@gmail.com>
2026-07-15 18:35:52 +0900

    배틀씬 돌입

--- Assets/01.Scri/PlayerManager.cs
+++ Assets/01.Scri/PlayerManager.cs
@@ -7,10 +7,6 @@ public class PlayerManager : MonoBehaviour
 
     public Tride PlayerData {  get; private set; }
 
-    
-    
-   
-
     private void Awake()
     {
         if (instance == null)
@@ -28,7 +24,17 @@ public class PlayerManager : MonoBehaviour
         PlayerData = Data.Clone();
     }
 
+    public void TakeDamage()
+    {
+        //회피율을 이용한 회피 여부
+        //크리트컬 여부 계산후 공격력에 더해주기 (데미지 2배)
+        //데미지 입을시 체력에 방어력 뺀 만큼 공격력만큼 줄이기
+    }
 
+    public void Attack()
+    {
+
+    }
 
 
 }
