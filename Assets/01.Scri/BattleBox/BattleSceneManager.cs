59140e75a82f3f064cc65cc4827d0b497430effe
seongwoo1246 <seongau717@gmail.com>
2026-07-19 21:58:09 +0900

    게임 플레이어 정리

--- Assets/01.Scri/BattleBox/BattleSceneManager.cs
+++ Assets/01.Scri/BattleBox/BattleSceneManager.cs
@@ -1,8 +1,10 @@
-using UnityEngine;
-using UnityEngine.UI;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 
@@ -37,18 +39,24 @@ public class BattleSceneManager : MonoBehaviour
 {
    public static BattleSceneManager instance;
 
+    [SerializeField] GameObject GameOver1;
+
     [SerializeField] TextMeshProUGUI resultYut;
     [SerializeField] TextMeshProUGUI TurnCount;
     [SerializeField] TextMeshProUGUI moCount;
     [SerializeField] TextMeshProUGUI yutCount;
     [SerializeField] TextMeshProUGUI yutname;
     [SerializeField] TextMeshProUGUI First;
+    [SerializeField] TextMeshProUGUI GainMoney;
+    [SerializeField] TextMeshProUGUI Break;
+    [SerializeField] TextMeshProUGUI Tip;
     
     [SerializeField] Button mo;
     [SerializeField] Button yut;
     [SerializeField] Button elseyut;
     [SerializeField ] Button MyChar;
     [SerializeField ] Button EnemyChar;
+    [SerializeField ] Button GoLobby;
 
 
     public GameObject playData1;
@@ -81,6 +89,7 @@ public class BattleSceneManager : MonoBehaviour
 
     private void Start()
     {
+        GameOver1.SetActive(false);
         Turn = 0;
         TurnCount.text =$"경과 턴 : {Turn}";
         FirstStart();
@@ -89,23 +98,122 @@ public class BattleSceneManager : MonoBehaviour
     }
 
 
-    public virtual void TakeDamage()
+    public virtual void TakeDamage(float miss, float hp, int damage, int depence)
     {
-        //회피율을 이용한 회피 여부
+       if(Random.value < miss )
+        {
+            //공격 회피 문구
+            return;
+            
+        }
+       else
+        {
+            //공격회피 실패 문구
+           int Damage = Mathf.Max(0, damage-depence);
+
+            hp-=Damage;
+
+            //게임 오버처리
+            if(hp<=0)
+            {
+                hp = 0;
+                GameOver();
+            }
+
+            
+        }
+
+
+    }
+
+    public int Attack( float critical ,int damage)
+    {
+        int finalDamage = damage;
+        if(Random.value<critical)
+        {
+            finalDamage = damage * 2;
+        }
+        return finalDamage;   
+    }
+
+    public void UseSkill(float block, float luck)
+    {
+        if(Random.value + block < luck)
+        {
+            //스킬 성공
+        }
         
-        //체력 -= (데미지(어택함수) - 방어력)
     }
 
-    public virtual void Attack()
+    public  void Heal(float hp , int heal)
     {
-        //행운,방해율 계산 효과 발동 여부 = 따로 함수로 만들까?
-        //크리트컬 여부 계산후 공격력에 더해주기 (데미지 2배)
-        //
+        hp += heal;
     }
 
-    public virtual void Heal()
+    public void GoToLobby()
     {
-        //체력 +=힐량 
+        ScenesM.instance.LoadScenes(scenetpye.Lobby);
+    }
+
+    public void GameOver()
+    {
+        GainMoney.text = TrainingUi.Instance.haveMoney + GainMoneys().ToString();
+        if (PlayerManager.Instance.PlayerData.hp == 0)
+            Break.text = "패배하셨군요 다음에 도전해 보세요.";
+        else
+        { Break.text = "승리를 축하드립니다.  상대 종족을 사용할 수 있게 되었습니다."; }
+        GiveTip();
+
+            GameOver1.SetActive(true);
+
+    }
+
+    public int GainMoneys()
+    {
+
+        int gainM = Random.Range(500, 2000);
+        
+        return gainM;
+    }
+
+    public void GiveTip()
+    {
+     int tipNum  = Random.Range(0, 3);
+
+        if (PlayerManager.Instance.PlayerData.hp == 0)
+        {
+            switch (tipNum)
+            {
+                case 0:
+                    Tip.text = " 좀 더 훈련해서 도전 해보는 걸 추천합니다.";
+                    break;
+                case 1:
+                    Tip.text = " 아쉽게도 이번에는 운이 없었군요. 다음에는 이길 수 있을 겁니다.";
+                    break;
+
+                case 2:
+                    Tip.text = " ai가 그렇게 똑똑하지 않습니다. 그 부분을 잘 노리면 이길 수 있을 겁니다.";
+                    break;
+            }
+        }
+        else
+        {
+            switch (tipNum)
+            {
+                case 0:
+                    Tip.text = "오? 이걸 이기네 축하합니다. 다음에는 좀 더 악마 같은 친구를 데려 오겠습니다.";
+                    break;
+                case 1:
+                    Tip.text = "그만해 그녀석에 HP는 이미 zero라고!!!";
+                    break;
+
+                case 2:
+                    Tip.text = "그녀석은 만든 적들 중에서 가장 약한 녀석이였다. 다음 적은 이렇게 쉽지 않을 걸!!!";
+                    break;
+            }
+        }
+
+       
     }
 
     private IEnumerator FalseText (TextMeshProUGUI Text)
