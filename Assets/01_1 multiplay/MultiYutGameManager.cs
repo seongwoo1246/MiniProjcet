2b899751ff0adcf8361c07a92c44285d8556570f
seongwoo1246 <seongau717@gmail.com>
2026-08-05 21:16:20 +0900

    .

---
+++ Assets/01_1 multiplay/MultiYutGameManager.cs
@@ -0,0 +1,35 @@
+//using UnityEngine;
+//using UnityEngine.UI;
+//using Photon.Pun;
+//using Photon.Realtime;
+
+//public class MultiYutGameManager : MonoBehaviourPunCallbacks
+//{
+//  public static MultiYutGameManager instance;
+
+//    [Header("UI ¿¬°á¿ë")]
+//    public Button yutThrow;
+//    public Text turnInfoText;
+
+//    public int currentTurnPlayerActorNumber;
+
+
+
+//    private void Awake()
+//    {
+//        instance = this;
+//    }
+
+
+//    private void Start()
+//    {
+        
+//    }
+
+
+//    public void SetTurn(int playerActorNumer)
+//    {
+//       // PhotonView.RPC("RPC_UpdateTurn", RpcTarget.AllBuffered, playerActorNumer);
+//    }
+
+//}
